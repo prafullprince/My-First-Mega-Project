4b0a8275c83e873d20225701b236973363d182f7
prafullprince <prafullprince12@gmail.com>
2024-04-25 17:30:12 +0530

    c

--- src/components/Common/ConfirmationModal.jsx
+++ src/components/Common/ConfirmationModal.jsx
@@ -4,7 +4,6 @@ export default function ConfirmationModal({ modalData }) {
   return (
     <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
       <div className="w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
-      <p></p>
         <p className="text-2xl font-semibold text-richblack-5">
           {modalData?.text1}
         </p>
